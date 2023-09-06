bbdb8f76e8626706bd2d768e71b1bfb3b1273a55
R5G1 <vlad.klimachevski@gmail.com>
2023-09-06 12:58:15 +0300

    feat: add burge menu

--- src/pages/header/burger.js
+++ src/pages/header/burger.js
@@ -4,6 +4,11 @@
 /* eslint-disable consistent-return */
 
 const btnBurger = document.querySelector('.menu__bureger');
+const btnContent = document.querySelector('.navigation');
+const bodyContent = document.querySelector('body');
+
 btnBurger.addEventListener('click', () => {
     btnBurger.classList.toggle('active');
+    btnContent.classList.toggle('active');
+    bodyContent.classList.toggle('active');
 });
