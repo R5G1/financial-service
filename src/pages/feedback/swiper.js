2605b473c3db4c7f98f45ce2b8a006f900eb1628
R5G1 <vlad.klimachevski@gmail.com>
2023-09-13 15:13:57 +0300

    refactor: change style of the project

--- src/pages/feedback/swiper.js
+++ src/pages/feedback/swiper.js
@@ -5,29 +5,29 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 Swiper.use([Navigation, Pagination]);
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.swiper', {
 
-        breakpoints: {
-            420: {
-                slidesPerView: 1,
-            },
-            992: {
-                slidesPerView: 2,
-            },
-            1200: {
-                slidesPerView: 3,
-            },
+    breakpoints: {
+      420: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
 
-        },
+    },
 
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'fraction',
-        },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
 
-        navigation: {
-            nextEl: '.feedback__btn-r',
-            prevEl: '.feedback__btn-l',
-        },
-    });
+    navigation: {
+      nextEl: '.feedback__btn-r',
+      prevEl: '.feedback__btn-l',
+    },
+  });
 });
